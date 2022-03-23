957ae7d7e744432f37436ababa1914b548adb852
Sergio Pérez Montero <98036930+sperezmont@users.noreply.github.com>
2022-03-23 09:34:07 +0100

    Add files via upload

--- proyects/ABUMIP/bmb-fmb_yelmo-v1.75.sh
+++ proyects/ABUMIP/bmb-fmb_yelmo-v1.75.sh
@@ -1,5 +1,6 @@
 # BMB-fmb tests with Yelmo v1.75
 # Aim: study the effect of bmb_gl_method and fmb on ABUC and ABUM-floating
+# How to run --> nohup bmb-fmb_yelmo-v1.75.sh &
 yelmox_path=/home/sergio/entra/yelmo_vers/v1.75/yelmox/
 
 exps=$(echo "abuc" "abum")
@@ -7,7 +8,7 @@ bmbmeth=$(echo "fcmp" "pmp" "nmp")
 fmbscale=$(echo 0.0 1.0 10.0)
 
 fldr=output/ismip6/bmb-fmb_yelmo-v1.75/
-path_restart=${yelmox_path}/bmb-dtt_yelmo-v1.75/restart/
+path_restart=${yelmox_path}/output/ismip6/bmb-dtt_yelmo-v1.75/restart/
 runopt='-r'
 paropt="ytopo.kt=1.0e-3"
 
@@ -20,6 +21,7 @@ for k in $exps; do
         for j in $fmbscale; do
             echo "#### ${k}_${i}_fmb${j}"
             file_restart=/${path_restart}/${i}/yelmo_restart.nc
+            echo "${file_restart}"
             ./runylmox ${runopt} -e ismip6 -n par/yelmo_ismip6_Antarctica.nml -o ${fldr}/${k}_${i}_fmb${j} -p abumip.scenario=${k} ctrl.run_step="abumip_proj" yelmo.restart=${file_restart} abumip_proj.scenario="ctrl" tf_cor.name="dT_nl" marine_shelf.gamma_quad_nl=14500 isostasy.method=0 ${paropt} ydyn.ssa_lat_bc="floating" ytopo.bmb_gl_method=${i} ytopo.fmb_scale=${j}
             
             while pgrep -x "yelmox_ismip6.x" > /dev/null; do
@@ -28,3 +30,5 @@ for k in $exps; do
 done
 done
 done
+
+
